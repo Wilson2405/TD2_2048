99a6d03c12b7a88f76cbd24dac485249b415be40
Wilson2405 <wil.abdimous@gmail.com>
2026-01-31 18:32:05 +0100

    try and catch

--- main.cpp
+++ main.cpp
@@ -3,17 +3,24 @@
 #include"Jeux.h"
 
 int main(){
-    std::srand(std::time(nullptr));
-    Grille g1(5);
-    Jeux game(g1);
-    game.start();
-    std::string answer = "y";
-    while(answer == "y" ||answer == "Y" || answer == "yes"){
-        std::cout << "Nouvelle partie?[y/N] ";
-        std::cin >> answer;
-        if(answer == "y" || answer == "yes" || answer == "Y")
-            game.restart();
+    try 
+    {
+        std::srand(std::time(nullptr));
+        int size = 5;
+        Grille g1(size);
+        Jeux game(g1);
+        game.start();
+        std::string answer = "y";
+        while(answer == "y" ||answer == "Y" || answer == "yes"){
+            std::cout << "Nouvelle partie?[y/N] ";
+            std::cin >> answer;
+            if(answer == "y" || answer == "yes" || answer == "Y")
+                game.restart();
+        }
+    } 
+    catch (const std::runtime_error& e) 
+    {
+        std::cerr << "Error : " << e.what() << std::endl;
     }
-    
     return 0;
 }
